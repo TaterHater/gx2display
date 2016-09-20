8b66c550bd42e4561a440e565064da30c39c548f
Nick B <nick_beeson@yahoo.com>
2016-09-20 06:28:40 -0600

    first commit
    bugs: shows all nodes as up, even if its not

---
+++ Assets/scripts/Node.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using UnityEngine.UI;
+
+public class Node : MonoBehaviour {
+
+    public string[] pingnames;
+
+   public Text txt;
+    public float offset;
+    public GameObject pin;
+    public int isDown;
+    public int size;
+    private int sitesDown;
+    IEnumerator Offset(float t)
+    {
+        yield return new WaitForSeconds(t);
+    }
+    IEnumerator pause()
+    {
+        yield return new WaitForEndOfFrame();
+    }
+    IEnumerator Timer()
+    {
+       // while (true)
+        {
+            isDown = 0;
+            sitesDown = 0;
+            size = pingnames.Length;
+            for (int i = 0; i < size; i++)
+            {
+                Debug.Log("pinging " + pingnames[i]);
+              
+                StartCoroutine(CheckConnection(pingnames[i]));
+                pause();
+            }
+            
+            
+            yield return new WaitForSeconds(10);
+        }
+
+    }
+
+    IEnumerator CheckConnection(string ip)
+    {
+        const float timeout = 5f;
+        float startTime = Time.timeSinceLevelLoad;
+        UnityEngine.Ping ping = new UnityEngine.Ping(ip);
+
+       // while (true)
+        {
+            Debug.Log("Checking network...");
+            while (!ping.isDone)
+            {
+                Debug.Log("time since level load "+startTime);
+
+
+                if (Time.timeSinceLevelLoad - startTime > timeout)
+                {
+                    Debug.Log("No network at " + ip + " (" + this.name + ")");
+                    pin.GetComponent<Renderer>().material.color = Color.red;
+                    isDown++;
+                    sitesDown++;
+                    yield break;
+
+                }
+                else
+                {
+                    Debug.Log("Network available.");
+                    if (isDown == 0)
+                    {
+
+                        pin.GetComponent<Renderer>().material.color = Color.green;
+                    }
+                    else
+                    {
+                        isDown--;
+                    }
+
+                    yield break;
+                }
+
+            }
+            if (Time.timeSinceLevelLoad - startTime > timeout)
+            {
+                Debug.Log("No network at " + ip + " (" + this.name + ")");
+                pin.GetComponent<Renderer>().material.color = Color.red;
+                isDown++;
+                sitesDown++;
+                yield break;
+
+            }
+            yield return new WaitForEndOfFrame();
+    }
+    }
+    void Start()
+    {
+        sitesDown = 0;
+        Offset(offset);
+      //  StartCoroutine(Timer());
+        
+    }	
+    public void pingSites()
+    {
+        sitesDown = 0;
+        Offset(offset);
+        StartCoroutine(Timer());
+    }
+
+    public int getSiteTotal()
+    {
+        return size;
+    }
+    public int GetSitesDown()
+    {
+        return sitesDown;
+    }
+    
+}
+
---
+++ Assets/scripts/SceneController.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+public class SceneController : MonoBehaviour {
+
+    public Camera[] cameras;
+    public int iter;
+    public Text clock;
+    public Text siteInfo;
+//arrays of sites
+    public GameObject[] USSites;
+    public GameObject[] AUSites;
+    public GameObject[] UKSites;
+    bool ispinging = false;
+
+   
+
+	// Update is called once per frame
+	void Update () {
+        DateTime theClock = DateTime.Now;
+        float time = Time.time;
+        float t = time % 100;
+
+
+
+        string min = "";
+        if (theClock.Minute < 10) min = "0" + min;
+        else min = theClock.Minute+"";
+        //the following tests the modulo of time and decides based on the timer, what area to display info for.
+        //US View and info
+        if (t<33.3)
+        {
+            
+            int ussitesdown = 0;
+            int ussites = 0;
+            
+            for(int i =0; i< USSites.Length; i++)
+            {
+                ussitesdown += USSites[i].GetComponent<Node>().GetSitesDown();
+                ussites += USSites[i].GetComponent<Node>().getSiteTotal();
+                if (t < 0.5 && t > 0 || t<30 && t>29.5)
+                {
+                    USSites[i].GetComponent<Node>().pingSites();
+                    ispinging = true;
+                }
+                
+            }
+           
+            clock.text = theClock.Hour + ":" + min;
+            cameras[1].gameObject.SetActive(false);
+            cameras[2].gameObject.SetActive(false);
+            cameras[0].gameObject.SetActive(true);
+            siteInfo.text = "Total sites: " + ussites + "\nSites down: " + ussitesdown;
+            
+        }
+        //EU view and info
+        else if (t > 33.33 && t< 66.33)
+        {
+            
+            int uksitesdown = 0;
+            int uksites = 0;
+            for (int i = 0; i < UKSites.Length; i++)
+            {
+                uksitesdown += UKSites[i].GetComponent<Node>().GetSitesDown();
+                uksites += UKSites[i].GetComponent<Node>().getSiteTotal();
+               
+
+            }
+            siteInfo.text = "Total sites: " + uksites + "\nSites down: " + uksitesdown;
+
+            if (theClock.Hour + 7 > 24)
+            {
+                clock.text = (theClock.Hour -17) + ":" + min;
+            }
+            else clock.text = (theClock.Hour+7) + ":" + min;
+
+            for (int i = 0; i < UKSites.Length; i++)
+            {
+                uksitesdown += UKSites[i].GetComponent<Node>().GetSitesDown();
+                uksites += UKSites[i].GetComponent<Node>().getSiteTotal();
+                if (t < 33.5 && t > 33 || t < 66.33 && t > 60)
+                {
+                    UKSites[i].GetComponent<Node>().pingSites();
+                    ispinging = true;
+                }
+
+            }
+
+
+            cameras[1].gameObject.SetActive(false);
+            cameras[2].gameObject.SetActive(true);
+            cameras[0].gameObject.SetActive(false);
+        }
+        //AU view and info
+        else
+        {
+            if (theClock.Hour + 16 > 24)
+            {
+                clock.text = (theClock.Hour - 8) + ":" + min;
+            }
+            else
+            clock.text = (theClock.Hour + 16) + ":" + min;
+
+
+            int ausites = 0 ;
+            int ausitesdown = 0;
+            for (int i = 0; i < AUSites.Length; i++)
+            {
+                ausitesdown += AUSites[i].GetComponent<Node>().GetSitesDown();
+                ausites += AUSites[i].GetComponent<Node>().getSiteTotal();
+               
+                if (t < 67.5 && t > 66.33 || t < 100 && t > 99.3)
+                {
+                    AUSites[i].GetComponent<Node>().pingSites();
+                    ispinging = true;
+                }
+
+            }
+            siteInfo.text = "Total sites: " + ausites + "\nSites down: " + ausitesdown;
+            cameras[1].gameObject.SetActive(true);
+            cameras[0].gameObject.SetActive(false);
+            cameras[2].gameObject.SetActive(false);
+        }
+      
+
+	}
+}
